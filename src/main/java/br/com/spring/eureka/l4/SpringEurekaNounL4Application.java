fdac424dfcb54e132c6416ce9a61238d9e3ba6d7
Ailton Muniz Santos Junior <ailton.msj@gmail.com>
2018-12-22 13:57:14 -0200

    Initial commit

---
+++ src/main/java/br/com/spring/eureka/l4/SpringEurekaNounL4Application.java
@@ -0,0 +1,14 @@
+package br.com.spring.eureka.l4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEurekaNounL4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaNounL4Application.class, args);
+	}
+
+}
+
